fc23829fd75bd6c415a7b5db99bd10341d82d85f
artlu99 <alu99.sm@gmail.com>
2025-05-29 19:35:02 -0400

    count replies, recasts, likes

--- src/frontend/components/CastFooter.tsx
+++ src/frontend/components/CastFooter.tsx
@@ -1,3 +1,7 @@
+import {
+	useReactionsQuery,
+	useRepliesQuery,
+} from "../hooks/queries/useHubQuery";
 import { useFrameSDK } from "../hooks/use-frame-sdk";
 import { useLocalStorageZustand } from "../hooks/use-zustand";
 import type { HydratedCast } from "../types";
@@ -12,6 +16,17 @@ export const CastFooter = ({ cast }: CastFooterProps) => {
 
 	const warpcastUrl = `https://farcaster.xyz/${cast.user.username}/${cast.hash.slice(0, 10)}`;
 
+	const likesQuery = useReactionsQuery(cast.user.fid, cast.hash, "Like");
+	const likes = likesQuery.data?.allReactionsData[cast.hash] ?? [];
+	const numLikes = likes.length;
+
+	const recastsQuery = useReactionsQuery(cast.user.fid, cast.hash, "Recast");
+	const recasts = recastsQuery.data?.allReactionsData[cast.hash] ?? [];
+	const numRecasts = recasts.length;
+
+	const repliesQuery = useRepliesQuery(cast.user.fid, cast.hash);
+	const numReplies = repliesQuery.data ?? 0;
+
 	return (
 		<div className="card-footer bg-base-100 text-center mr-8">
 			<div className="flex justify-between items-center">
@@ -20,21 +35,33 @@ export const CastFooter = ({ cast }: CastFooterProps) => {
 					className="btn btn-sm btn-ghost text-sm text-base-content/50"
 					onClick={() => composeCast(cast.user.fid, cast.hash)}
 				>
-					<i className="ri-chat-4-line" />
+					<i className="ri-chat-4-line">
+						<span className="font-mono  ml-1">
+							{numReplies > 99 ? "99+" : numReplies > 0 ? numReplies : ""}
+						</span>
+					</i>
 				</button>
 				<button
 					type="button"
 					className="btn btn-sm btn-ghost text-sm text-base-content/50"
 					onClick={() => openUrl(warpcastUrl)}
 				>
-					<i className="ri-loop-left-line" />
+					<i className="ri-loop-left-line">
+						<span className="font-mono ml-1">
+							{numRecasts > 99 ? "99+" : numRecasts > 0 ? numRecasts : ""}
+						</span>
+					</i>
 				</button>
 				<button
 					type="button"
 					className="btn btn-sm btn-ghost text-sm text-base-content/50"
 					onClick={() => openUrl(warpcastUrl)}
 				>
-					<i className="ri-heart-line" />
+					<i className="ri-heart-line">
+						<span className="font-mono ml-1">
+							{numLikes > 99 ? "99+" : numLikes > 0 ? numLikes : ""}
+						</span>
+					</i>
 				</button>
 				{contextFid &&
 				showTipButtons &&
---
+++ src/frontend/hooks/queries/useHubQuery.ts
@@ -0,0 +1,59 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetcher } from "itty-fetcher";
+import type { Message } from "@farcaster/core";
+
+const client = fetcher({ base: "https://pop.farcaster.xyz:3381" });
+const MAX_REACTIONS_PAGE_SIZE = 100;
+
+export const FARCASTER_EPOCH = 1609459200;
+
+export const useReactionsQuery = (
+	fid: number,
+	castHash: string,
+	reactionType: "Like" | "Recast" = "Like",
+) => {
+	return useQuery({
+		queryKey: ["hub-reactions", fid, castHash, reactionType],
+		queryFn: async () => {
+			const allReactionsData: Record<string, number[]> = {};
+			const lastReactedTimes: Record<string, number> = {};
+
+			const res = await client.get<{ messages: Message[] }>(
+				`/v1/reactionsByCast?${new URLSearchParams({
+					target_fid: fid.toString(),
+					target_hash: castHash,
+					reaction_type: reactionType,
+					page_size: MAX_REACTIONS_PAGE_SIZE.toString(),
+				})}`,
+			);
+
+			allReactionsData[castHash] =
+				res?.messages.map((m) => m.data?.fid ?? 0) || [];
+			lastReactedTimes[castHash] =
+				res?.messages.reduce((max, m) => {
+					const timestamp = ((m.data?.timestamp ?? 0) + FARCASTER_EPOCH) * 1000;
+					return Math.max(max, timestamp);
+				}, 0) ?? 0;
+			return { allReactionsData, lastReactedTimes };
+		},
+		refetchInterval: 60 * 1000, // Auto-refresh every 60 seconds
+	});
+};
+
+export const useRepliesQuery = (fid: number, castHash: string) => {
+	return useQuery({
+		queryKey: ["hub-replies", fid, castHash],
+		queryFn: async () => {
+			const res = await client.get<{ messages: Message[] }>(
+				`/v1/castsByParent?${new URLSearchParams({
+					fid: fid.toString(),
+					hash: castHash,
+					page_size: MAX_REACTIONS_PAGE_SIZE.toString(),
+				})}`,
+			);
+
+			return res?.messages.length ?? 0;
+		},
+		refetchInterval: 60 * 1000, // Auto-refresh every 60 seconds
+	});
+};
